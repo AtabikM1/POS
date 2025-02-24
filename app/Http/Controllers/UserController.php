0480a976b694261253c78c8e6bdd860d6962a35a
AtabikM1 <mutawakilatabik@gmail.com>
2025-02-24 21:58:00 +0700

    adding : controller, seeder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function show($id, $name)
-    {
-        return view('user.profile', compact('id', 'name'));
+    public function profile($id, $name) {
+        return view('user.profile', ['id' => $id, 'name' => $name]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,30 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-
-
-
-Route::prefix('category')->group(function () {
-    Route::get('/', [ProductController::class, 'index'])->name('category.index');
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/', function(){
+    return view('welcome');
 });
 
+Route::get('level', [LevelController::class,'index']);
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('user.show');
 
+// Route::prefix('category')->group(function () {
+//     Route::get('/', [ProductController::class, 'index'])->name('category.index');
+//     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+//     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+//     Route::get('/home-care', [ProductController::class, 'homeCare']);
+//     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+// });
 
-Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
+// Route:: get('/', function () {
+// return view( 'welcome');
+// });
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get ('/kategori', [KategoriController::class,'index' ]);
+// Route::get ('/user', [UserController::class,'index']);
