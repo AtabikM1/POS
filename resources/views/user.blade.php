66c93d17a34b94cb6c76862e1f6b1e0eee88f9c1
AtabikM1 <mutawakilatabik@gmail.com>
2025-02-24 23:27:53 +0700

    done : all

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Data User</title>
 </head>
+
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
@@ -16,14 +15,14 @@
             <th>ID Level Pengguna</th>
         </tr>
         @foreach ($data as $d)
-        <tr>
-            <td>{{$D->user_id}}</td>
-            <td>{{$D->username}}</td>
-            <td>{{$D->nama}}</td>
-            <td>{{$D->level}}</td>
-        </tr>
-        
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+            </tr>
         @endforeach
     </table>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+| blab;abalsdansldas
+*/
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::get('/', function(){
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('level', [LevelController::class,'index']);
-
-
-// Route::prefix('category')->group(function () {
-//     Route::get('/', [ProductController::class, 'index'])->name('category.index');
-//     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-//     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-//     Route::get('/home-care', [ProductController::class, 'homeCare']);
-//     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-// });
-
-// Route:: get('/', function () {
-// return view( 'welcome');
-// });
-// Route::get('/level', [LevelController::class, 'index']);
-// Route::get ('/kategori', [KategoriController::class,'index' ]);
-// Route::get ('/user', [UserController::class,'index']);
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
